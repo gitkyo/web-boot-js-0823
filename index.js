a63fb59788b16b786a162f2b8f992b491c61f907
Pier <p.delaunay1@gmail.com>
2022-08-22 11:48:19 +0400

    test de jquery

--- index.js
+++ index.js
@@ -1,41 +1,94 @@
-//doc : https://fr.javascript.info/fetch
-// objectif : fecth data on https://api.github.com/users/gitkyo
 
-let user = 'gitkyo';
-let url = `https://api.github.com/users/${user}`;
+// verifier le chargement complet du DOM de la page avant d'éxecuter le code JS
+// https://developer.mozilla.org/fr/docs/Web/API/Window/DOMContentLoaded_event
+window.addEventListener("DOMContentLoaded", (event) => {
 
 
-async function getAvatarFromGitUrl(){
+    
+    //doc : https://fr.javascript.info/fetch
+    // objectif : fecth data on https://api.github.com/users/gitkyo
 
-    //await car fetch est un methode qui renvoi une promesse
-    let response = await fetch(url);
+    let user = 'gitkyo';
+    let url = `https://api.github.com/users/${user}`;
 
-    //vérification de la réponse via le code status 200
-    if(response.status === 200){
 
-        //on récupère le contenu de la réponse
-        let data = await response.json();
+    async function getAvatarFromGitUrl(){
 
-        //affichage
-        // console.log(data);
-        let imSrc = data.avatar_url;
-        return imSrc;
+        //await car fetch est un methode qui renvoi une promesse
+        let response = await fetch(url);
+
+        //vérification de la réponse via le code status 200
+        if(response.status === 200){
+      
+            //on récupère le contenu de la réponse
+            let data = await response.json();
+            
+            //affichage
+            // console.log(data);
+            let imSrc = data.avatar_url;
+            return imSrc;
+
+        }
 
     }
 
-}
+
+    //exemple de récuperation de données depuis une promess avec then
+    getAvatarFromGitUrl().then(function(imSrc){
+        // console.log(imSrc);
+        let img = document.createElement('img');    
+        img.src = imSrc;
+        img.classList.add("avatar");
+        document.body.appendChild(img);
 
 
-//exemple de récuperation de données depuis une promess avec then
-getAvatarFromGitUrl().then(function(imSrc){
-    // console.log(imSrc);
-    let img = document.createElement('img');    
-    img.src = imSrc;
-    img.classList.add("avatar");
-    document.body.appendChild(img);
-});
+        testjQuery();
+    });
+
+
+    function testjQuery(){
+     
+        //detect event
+        $('.avatar').on('click', function(){            
+            
+            //this correspond à l'élément sur lequel on a cliqué (l'élem courant)
+            $(this).hide()
+        });
+
+        //ecriture jquery
+        $('h1').css('color', 'black');
+
+
+        //test d'appels asynchrone avec jquery 
+        //API url : https://tenor.com/gifapi/documentation#quickstart-search
+        $.ajax({            
+            url: "https://g.tenor.com/v1/search?q=jquery&key=LIVDSRZULELA&limit=1",            
+        })
+        .done(function( response ) {
+
+            //ici mes instruction pour traiter la réponse
+            console.log(response);
+
+            //on récupere une url dans la réponse
+            let media = response.results[0].media[0].gif.url;
+
+            //on créer une element img avec l'url
+            let img = document.createElement('img');
+            img.src = media;
+            img.classList.add("gif");
+          
+            //ajout dans le dom
+            document.body.appendChild(img);
+
+            //ajout du comportement draggable sur l'image avec jquery ui
+            $('.gif').draggable();
+            
+        });
+
+
+    }
 
 
 
-
+ })
 
